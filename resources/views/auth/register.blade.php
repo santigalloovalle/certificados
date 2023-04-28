75c958a84a1a385879a0c971177d8e87f3b7f8c4
GuascaC <mauricioguasca11@gmail.com>
2023-04-28 13:59:19 -0500

    28/04/CG

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,10 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="registro">
+<div class="headerIng">
+    <div>
+      <img src="./img/LogoUECCB.png" alt="Logo Uniempresarial y logo Cámara de comercio">
+    </div>
+  </div>
+<section class="registro"></section>
     <br><br>
+    <div class=" col-form-label text-md-center text-blue ">
     <h1>Regístrate</h1>
     <!--Formulario de registro-->
+    </div>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -26,22 +33,27 @@
                             </div>
                         </div>
                         <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Cédula de ciudadania') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="" type="number" class="form-control @error('') is-invalid @enderror" name="" value="{{ old('') }}" required autocomplete="" autofocus>
-                                <div class="row mb-3">
-                                    <label><i class="fa-solid fa-briefcase"></i> Rol </label><br>
-                                    <select id="iRol">
-                                      <option value="0">Seleccione Cargo</option>
-                                      <option value="1">Administrativos</option>
-                                      <option value="2">Profesores de planta</option>
-                                      <option value="3">Profesores de catedra</option>
-                                      <option value="4">Aprendices</option>
-                                      <option value="5">Talleristas bienestar</option>
-                                    </select>
-                                </div>
-                                <div class="row mb-3">
+                            <label for="CC" class="col-md-4 col-form-label text-md-end">{{ __('Cédula de ciudadania') }}</label>
+                            
+                            <div class="col-md-6">    
+                                <input id="CC" type="number" class="form-control @error('') is-invalid @enderror" name="" value="{{ old('') }}" required autocomplete="" autofocus>
+                            </div> 
+                        </div> 
+                        <div class="row mb-3">
+                            <label for="" class="col-md-4 col-form-label text-md-end">{{ __('Rol') }}</label>
+                            
+                            <div class="col-md-6"> 
+                            <select class="form-control">  
+                                <option value="0"></option>
+                                <option value="1">Administrativos</option>
+                                <option value="2">Profesores de planta</option>
+                                <option value="3">Profesores de catedra</option>
+                                <option value="4">Aprendices</option>
+                                <option value="5">Talleristas bienestar</option>
+                             </select>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Correo Institucional') }}</label>
 
                             <div class="col-md-6">
@@ -76,10 +88,9 @@
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="botonGuardar">
+                        <div class="col-md-8 offset-md-4">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-blue">
                                     {{ __('Registrar') }}
                                 </button>
                             </div>
