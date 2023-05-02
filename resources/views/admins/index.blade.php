f0ebec1aa7e951670d65794fc8227e99eccc1101
GuascaC <mauricioguasca11@gmail.com>
2023-05-02 12:55:27 -0500

    02/05 CG

--- resources/views/admins/index.blade.php
+++ resources/views/admins/index.blade.php
@@ -1,100 +1,81 @@
 @extends('layouts.app')
 
 @section('content')
-
-  <!--Logo y cambiar contraseña-->  
-  <div class="headerIng">
-      <div>
-        <img src="/img/LogoUECCB.png" alt="Logo Uniempresarial y logo Cámara de comercio">
-      </div>
+<div class="headerIng">
+    <div>
+      <img src="./img/LogoUECCB.png" alt="Logo Uniempresarial y logo Cámara de comercio">
+    </div>
   </div>
-
-  <!--encabezado titulo-->  
-  <section class="sectionTitulo2">
-    <div class="divTitulo">
-      bienvenido al perfil administrador
-    </div>
-  </section>
-
-  <!--Corregir Front-->
-
-  <!--Menú perfil administrador-->
-
-  <!--Botón menú-->
-
-  <section class="row mt-5 text-center">
-    <div class="col-6">  
-      <div class="card bg-danger bg-gradient" style="width: 20rem;">
-        <i class="btn btn-red h5 fa-solid fa-clock-rotate-left"></i>
-        <div class="card-body">
-        <h5 class="card-title">historial de certificado</h5>
-        <a href="#" class="btn btn-primary $blue-900">Go somewhere</a>
+    <!--encabezado titulo-->
+    <section class="sectionTitulo2">
+        <div class="col-md-4 col-form-label text-md-end bg-red p-3 w-25 text-light">
+          <h2>Bienvenido al </h2>
         </div>
-      </div>
-    </div>
-
-    <div class="col-6">  
-      <div class="card bg-danger bg-gradient" style="width: 20rem;">
-        <i class="btn btn-red h5 fa-solid fa-solid fa-users"></i>
-        <div class="card-body">
-        <h5 class="card-title">Usuarios registrados</h5>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </section>
-    
-    <section class="row text-center">
-    <div class="col-6 mt-3">  
-      <div class="card bg-danger bg-gradient" style="width: 20rem;">
-        <i class="btn btn-red h5 fa-solid fa-file"></i>
-        <div class="card-body">
-        <h5 class="card-title">Generar certificado</h5>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    </div>
-    
-    <div class="col-6 mt-3">  
-      <div class="card bg-danger bg-gradient" style="width: 20rem;">
-        <i class="btn btn-red h5 fa-solid fa-rectangle-xmark"></i>
-        <div class="card-body">
-          <h5 class="card-title">Editar salario por cargos</h5>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    </div>
-  </section>
-  
-  {{-- <div class="row mb-0">
-    <div class="col-5-md-4 offset-md-2">
-      <button class="btn btn-red h5 fa-solid fa-clock-rotate-left"><a class="" href="./Historial.html"></a></button>
-      <p class="bs-black text- font-weight-bold">Historial de certificados</p>
-
-      <!--Botón menú-->
-      <div class="divAdministrador">
-      <button onclick="redirigir09()"><a href="./php/table.php"> <i class="fa-solid fa-users"></i></a></button>
-      <p>Usuarios registrados</p>
-      </div>
-      
-      <!--Botón menú-->
-      <div class="divAdministrador">
-        <button onclick="window.selecC.showModal();"><i class="fa-solid fa-file"></i></button>
-        <p>Generar certificado</p>
-      </div>
-
-      <!--Botón menú-->
-      <div class="divAdministrador">
-        <button onclick="redirigir11()"><a href="./ESalario.html"> <i class="fa-solid fa-pen-to-square"></i></a></button>
-        <p>Editar salarios por cargo</p>
+        <div class="col-md-4 col-form-label text-md-end bg-blue p-3 m-3 w-50 text-light">
+          <h2>Perfil administrador</h2>
         </div>
       </section>
+      <br><br><br><br><br><br><br><br><br><br><br><br><br> <!--Corregir Front-->
+        <!--Menú perfil administrador-->
 
-      <!--Modal que permite eligir al usuario si se quiere generar un certificado para alguien más o para si mismo-->
-      <dialog id="selecC" class="modulCer">
+
+        <div class="container">
+          <div class="row justify-content-center">
+              <div class="col-md-8">
+                  <div class="card">
+                      <div class="card-body">
+                                <button class="btn btn-red col-md-4 m-5 justify-content-end fa-solid fa-clock-rotate-left">
+                                <button class="btn btn-lg btn-red col-md-4 m-5 justify-content-end fa-solid fa-clock-rotate-left"> 
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+
+
+
+
+
+{{-- 
+        <div class="d-flex justify-content-center">
+          <div class="col-md-12">
+              <div class="card">
+                  <div class="card-body">
+        <!--Botón menú-->
+        <div class="row mb-0">
+            <div class="col-md-4 offset-md-8">
+            <button class="btn btn-red h-5 fa-solid fa-clock-rotate-left">
+                <a href="./Historial.html"></a>
+            </button>
+            <p class="bs-black text-blue font-weight-bold">Historial de certificados</p>
+            </div>
+        </div>
+        <!--Botón menú-->
+        <div class="row mb-0">
+          <div class="col-md-4 offset-md-8">
+            <button class="btn btn-red h-5 fa-solid fa-solid fa-users"><a href=""></a></button>
+            <p class="bs-black text-blue font-weight-bold">Usuarios registrados</p>
+            </div>
+        </div>
+    <!--Botón menú-->
+    <div class="row mb-0">
+      <div class="col-md-4 offset-md-8">
+            <button class="btn btn-red h-5 fa-solid fa-solid fa-file" onclick="window.selecC.showModal();"></button>
+            <p class="bs-black text-blue font-weight-bold">Generar certificado</p>
+            </div>
+        <!--Botón menú-->
+        <div class="row mb-0"></div>
+          <div class="col-md-4 offset-md-8">
+            <button class="btn btn-red h-5 fa-solid fa-solid fa-pen-to-square"><a href="./ESalario.html"></a></button>
+            <p class="bs-black text-blue font-weight-bold">Editar salarios por cargo</p>
+            </div>
+        </section>
+        <!--Modal que permite eligir al usuario si se quiere generar un certificado para alguien más o para si mismo-->
+        <dialog id="selecC" class="modulCer">
         <a href="./perfiladministrador.html"><i class="fa-solid fa-rectangle-xmark"></i></a>
         <h2>El certificado es para:</h2>
         <button class="paraMi" onclick="redirigir10()"> Para mí </button>
         <button class="otraP" onclick="redirigir13()"> Para otra persona</button>
-      </dialog>
-    </div> --}}
+        </dialog>
+    </div>--}}
 @endsection
