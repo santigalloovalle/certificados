bd15ae823fa77ce68b02923728bbc8f375298bbf
SsantiOg <egallo@uniempresarial.edu.co>
2023-05-17 14:23:11 -0600

    17/05 3:00 SG

--- resources/views/users/admins/edit.blade.php
+++ resources/views/users/admins/edit.blade.php
@@ -17,7 +17,7 @@
                         <div class="row mb-3">
                                 <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Nombre') }}</label>
                                 <div class="col-md-6">
-                                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{$people->doc}}" required autocomplete="name" autofocus>
+                                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{$users->name}}" required autocomplete="name" autofocus>
                                     @error('name')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
