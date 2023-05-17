f0bb06d6372f7d1b63da5d2fca5c2e0e0cbf3c71
SsantiOg <egallo@uniempresarial.edu.co>
2023-05-17 13:56:58 -0600

    17/05 3:00 SG

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -65,7 +65,7 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    @if( Auth::user()->name == 0 )
+                                    @if( Auth::user()->name == '0' )
                                         {{ Auth::user()->email }}
                                     @else
                                         {{ Auth::user()->name }}
