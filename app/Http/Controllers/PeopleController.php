4eae37bd72efa506d4fe188b72bc95243d8b29cb
CGuasca <mauricioguasca11@gmail.com>
2023-06-21 16:51:11 -0500

    Validaciones

--- app/Http/Controllers/PeopleController.php
+++ app/Http/Controllers/PeopleController.php
@@ -103,20 +103,6 @@ class PeopleController extends Controller
     public function update(Request $request, $id)
     {
 
-        $request->validate([
-            'doc' => 'required|unique:people|min:10|min:12',
-            't_doc' => 'required',
-            'date' => 'required',
-        ],[
-            'doc.required' => 'Se requiere número de documento',
-            't_doc.required' => 'Se requiere tipo de documento',
-            'date.required' => 'Se requiere fecha de expedición del documento',
-            'doc.min' => 'Caracteres mínimos:10',
-            'doc.max' => 'Caracteres máximos:12',
-        ]);
-
-        dd($request->all());
-
         $people = People::find($id);
         $users = User::find($id);
         $people->id_documents = $request->type;
