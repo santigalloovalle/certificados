f4117475ba40ac601d958cd276e381be45740c14
MiltonStivenPinzon1223 <miltonstivenpinzon@gmail.com>
2023-06-27 13:50:20 -0500

    auth en areas y cargos

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,6 +8,10 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    public function __construct()
+     {
+         $this->middleware('auth');
+     }
     public function index()
     {
         $posts = Post::all();
